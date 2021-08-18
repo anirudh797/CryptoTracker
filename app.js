5025274114d80a668d99fd3bb6af9fde51f7669b
Anirudh Kaptiyal <anirudh.kaptiyal77@gmail.com>
2021-08-18 10:53:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,22 @@ form.addEventListener('submit',(e) =>{
     
 
 });
+
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    return time;
+  }
+
+
 const fetchPrice = async(ctype)=>
 {
     let list = ctype.split('-');
@@ -36,7 +52,7 @@ const fetchPrice = async(ctype)=>
    const base = r.data.ticker.base;
    
    let target = r.data.ticker.target;
-    const time = r.data.timestamp;
+    const time = timeConverter(r.data.timestamp);
     // if(list[1]=="inr")
     // {price*=70;
     //  target = "INR"
